839ed2bf5085e074882f84649e6492f0bd52498b
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-06-19 11:14:14 +0200

    getUsersFromFirebase() / display user data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
@@ -9,9 +10,29 @@ import { User } from 'src/models/user.class';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-  user: User = new User();
+  user: User = new User(); // Kann vielleicht raus!
+  allUsers: any = [];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(private firestore: Firestore, public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.getUsersFromFirebase();
+  }
+
+  /**
+   * Retrieves user data from Firebase Firestore and onSnapshot() listens for real-time changes.
+   * map; Converts the documents in the snapshot into an array of user objects.
+   * allUsers = []; Updates the local variable with the new user data.
+   */
+  getUsersFromFirebase() {
+    let changes;
+    const collectionUsersRef = collection(this.firestore, 'users');
+    onSnapshot(collectionUsersRef, (snapshot) => {
+      changes = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); // ...doc.data = Spread Operator
+      this.allUsers = changes;
+      console.log('Users:', this.allUsers);
+    });
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
